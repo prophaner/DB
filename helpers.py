f40eec553fc29cc226d614fd7607ce2c83d9f9f6
Luis Ramos <lmramos89@gmail.com>
2019-01-21 12:27:16 -0500

    Create helpers.py

---
+++ helpers.py
@@ -0,0 +1,13 @@
+class ResidenceTypes(object):
+    def __init__(self):
+        self.id = "id"
+
+
+class ResidenceUse(object):
+    def __init__(self):
+        self.id = "id"
+
+
+class State(object):
+    def __init__(self):
+        self.id = "id"
